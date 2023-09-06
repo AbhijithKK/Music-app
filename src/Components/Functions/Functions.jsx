ef0d1c3d10035ed2b99f5f0e33ee270fe2c695ee
white <abhijithkkpurameri@gmail.com>
2023-09-06 14:58:45 +0530

    update version 1.1 to 1.2 and change some player code add duration bar

--- src/Components/Functions/Functions.jsx
+++ src/Components/Functions/Functions.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 import './Functions.css'
 import {FontAwesomeIcon} from '@fortawesome/react-fontawesome'
 import {faCirclePlay,faCirclePause,faBackward,faForward} from '@fortawesome/free-solid-svg-icons'
-export default function Functions({AudioPlay,isPlaying, NextSong ,PrevSong}) {
+export default function Functions({AudiPause,AudioPlay,isPlaying, NextSong ,PrevSong}) {
     
     
   return (
@@ -11,17 +11,18 @@ export default function Functions({AudioPlay,isPlaying, NextSong ,PrevSong}) {
 
    <FontAwesomeIcon icon={faBackward } style={{height:'20px',cursor:'pointer'}} />
    </button>
-   <button type='button' onClick={AudioPlay} style={{backgroundColor:'transparent',border:'0px'}}>
     {
-        isPlaying===false
-         ? <FontAwesomeIcon icon={faCirclePlay } style={{paddingLeft:'40px',paddingRight:'40px',height:'30px',
-        marginTop:'-5px',cursor:'pointer'}} />
-        :   <FontAwesomeIcon icon={faCirclePause } style={{paddingLeft:'40px',paddingRight:'40px',height:'30px',
+      isPlaying===false
+       ?<button type='button' onClick={AudioPlay} style={{backgroundColor:'transparent',border:'0px'}}>
+          <FontAwesomeIcon icon={faCirclePlay } style={{paddingLeft:'40px',paddingRight:'40px',height:'30px',
+           marginTop:'-5px',cursor:'pointer'}} />
+        </button>
+        :<button type='button' onClick={AudiPause} style={{backgroundColor:'transparent',border:'0px'}}>
+          <FontAwesomeIcon icon={faCirclePause } style={{paddingLeft:'40px',paddingRight:'40px',height:'30px',
         marginTop:'-5px',cursor:'pointer'}}  />
-
+    </button> 
     }
 
-   </button>
    <button type='button' onClick={NextSong} style={{backgroundColor:'transparent',border:'0px'}}>
    <FontAwesomeIcon icon={faForward } style={{height:'20px',cursor:'pointer'}} />
  
--- src/Components/Player/Player.js
+++ src/Components/Player/Player.js
@@ -1,53 +1,88 @@
-import React, {  useEffect, useState } from "react";
+import React, { useEffect, useState } from "react";
 import "./Player.css";
 import Functions from "../Functions/Functions";
 import SelectFile from "../SelectFile/SelectFile";
 function Player() {
-    const[song,setSongs]=useState([])
-    const[isPlaying,setIsplaying]=useState(false) 
-    const[name,setName]=useState(' ')
-    const [AudiRef,setAudiref]=useState(null)
-useEffect(()=>{
- 
+  const [song, setSongs] = useState([]);
+  const [isPlaying, setIsplaying] = useState(false);
+  const [name, setName] = useState("");
+  const [AudiRef, setAudiref] = useState(null);
+  const [index, setIndex] = useState(0);
+  const [currentTime, setCurrentTime] = useState(null);
+  const [currentDuration, setCurrentDuration] = useState(null);
+  const [isPaused, setIsPaused] = useState(false); 
+  useEffect(() => {}, [AudiRef]);
+  let i = 0;
+  const AudioPlay = (j) => {
+    console.log(isPaused);
+if (!isPaused) {
+  
+    console.log(i);
+    if (j) {
+      i = j;
+    }
+    if (song.length > 0 && song.length - 1 >= i) {
+      setName(song[i]?.name);
+      
+      let Audi = new Audio(URL.createObjectURL(song[i]));
+      setAudiref(Audi);
+      setIndex(i);
+      Audi.addEventListener("timeupdate", () => {
+        setCurrentDuration(Audi.duration);
+        setCurrentTime(Audi.currentTime);
+      });
+      Audi.addEventListener("ended", () => {
+        AudioPlay();
+      })
+        Audi.play();
+      i++;
+    } else {
+      i = 0;
+      AudioPlay();
+    }
+    setIsplaying(true);
+  }else{
+    AudiRef.play()
+    setIsplaying(true);
+  }
+  };
+  const AudiPause = () => {
+    setIsplaying(false);
+    AudiRef.pause();
+    setIsPaused(true)
+    // AudioPlay()
+  };
+  const NextSong = () => {
+    AudiRef.pause();
+    setIsPaused(false)
+    let i = index + 1;
+    AudioPlay(i);
+  };
+  const PrevSong = () => {
+    setIsPaused(false)
+    if (AudiRef) {
+      if (AudiRef.play()) {
+        AudiRef.pause();
+      }
+    }
+    let i = index - 1;
+    if (i < 0) {
+      i = song?.length - 1;
+    }
+    AudioPlay(i);
+  };
 
-},[AudiRef])
-let i=0
-    const AudioPlay=()=>{
-        console.log(i);
-          if (song.length > 0 && song.length-1>=i) {
-            setName(song[i]?.name)
-            let Audi=new Audio(URL.createObjectURL(song[i]))
-            setAudiref(Audi)
-            Audi.addEventListener('ended', () => { // Move event listener assignment here
-              AudioPlay();
-             
-            })
-            Audi.play() 
-            i++
-                     
-          }else{
-            i=0
-            AudioPlay()
-          }
-            setIsplaying(true)
-          
-       
-    }
-
-    const NextSong=()=>{
-        
-    }
-    const PrevSong=()=>{
-       
-    }
   return (
-    <div className="container" style={{
-        backgroundImage:'url(https://daily.jstor.org/wp-content/uploads/2023/01/good_times_with_bad_music_1050x700.jpg)',
-        backgroundRepeat:'no-repeat',
-        backgroundSize:'cover'
-        
-    }}>
-        <SelectFile setSongs={setSongs} />
+    <div
+      className="container"
+      style={{
+        backgroundImage:
+          "url(https://daily.jstor.org/wp-content/uploads/2023/01/good_times_with_bad_music_1050x700.jpg)",
+        backgroundRepeat: "no-repeat",
+        backgroundSize: "cover",
+      }}
+    >
+      <SelectFile setSongs={setSongs} />
 
       <div className="Player-body">
         <div className="imageDiv">
@@ -56,15 +91,31 @@ let i=0
             src="https://daily.jstor.org/wp-content/uploads/2023/01/good_times_with_bad_music_1050x700.jpg"
             alt=""
           />
-          
         </div>
-        <div className="texts">
-            {name}<br/>
-            <p><b>V1.1</b></p>
+        <div className="texts" style={{textAlign:name===''?'center':'' }}>
+          {name===''
+          ?"Click the play button and enjoy music" :name}
+          <br />
+          <p>
+            <b>V1.2</b>
+          </p>
         </div>
-        
+        <div className="progress-container">
+          <div
+            className="progress-bar"
+            style={{ width: `${(currentTime / currentDuration) * 100}%` }}
+          ></div>
+        </div>
+
         <div className="function">
-          <Functions song={song}  AudioPlay={AudioPlay} isPlaying={isPlaying} NextSong={NextSong} PrevSong={PrevSong} />
+          <Functions
+            AudiPause={AudiPause}
+            song={song}
+            AudioPlay={AudioPlay}
+            isPlaying={isPlaying}
+            NextSong={NextSong}
+            PrevSong={PrevSong}
+          />
         </div>
       </div>
     </div>
